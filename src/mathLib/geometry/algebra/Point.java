5be711bd6826ba9da1d3aa19819e72d1b3c6bdd0
javaNoviceProgrammer <meisam@192.168.2.10>
2018-07-19 21:02:05 -0400

    point modified

--- src/mathLib/geometry/algebra/Point.java
+++ src/mathLib/geometry/algebra/Point.java
@@ -2,4 +2,28 @@ package mathLib.geometry.algebra;
 
 public class Point {
 
+	private double x, y, z ;
+	int dim ;
+	
+	public Point(double x) { // 1D point
+		this.x = x ;
+		this.y = 0 ;
+		this.z = 0 ;
+		this.dim = 1 ;
+	}
+	
+	public Point(double x, double y) { // 2D point
+		this.x = x ;
+		this.y = y ;
+		this.z = 0 ;
+		this.dim = 2 ;
+	}
+	
+	public Point(double x, double y, double z) { // 3D point
+		this.x = x ;
+		this.y = y ;
+		this.z = z ;
+	}
+	
+	
 }
