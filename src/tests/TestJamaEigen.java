0c7fc047f931f36e391db247b18d927bd67cfe9b
javaNoviceProgrammer <meisam@c-73-211-150-252.hsd1.il.comcast.net>
2018-07-22 10:35:09 -0400

    some minor changes

--- src/tests/TestJamaEigen.java
+++ src/tests/TestJamaEigen.java
@@ -2,17 +2,26 @@ package tests;
 
 import Jama.EigenvalueDecomposition;
 import Jama.Matrix;
+import mathLib.numbers.Complex;
+import static mathLib.numbers.Complex.*;
 
 public class TestJamaEigen {
 	public static void main(String[] args) {
-		double[][] ent = new double[][] { { 1, -1.2, 3 }, { 2, 0, 7.2 }, { -1.3, 10.2, 0.5 } };
+		double[][] ent = new double[][] { { -1, 1 }, { 1, -1 } };
 		Matrix A = new Matrix(ent);
 		EigenvalueDecomposition eigen = new EigenvalueDecomposition(A);
-		for (double x : eigen.getRealEigenvalues()) {
-			System.out.println(x);
-		}
-		for (double y : eigen.getImagEigenvalues()) {
-			System.out.println(y);
+		// for (double x : eigen.getRealEigenvalues()) {
+		// System.out.println(x);
+		// }
+		// for (double y : eigen.getImagEigenvalues()) {
+		// System.out.println(y);
+		// }
+		double[] eigenReal = eigen.getRealEigenvalues();
+		double[] eigenImag = eigen.getImagEigenvalues();
+		Complex[] eigenComplex = new Complex[eigenReal.length];
+		for (int i = 0; i < eigenReal.length; i++) {
+			eigenComplex[i] = eigenReal[i] + j * eigenImag[i];
+			System.out.println(eigenComplex[i]);
 		}
 	}
 
