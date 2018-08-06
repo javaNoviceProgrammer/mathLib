9822655d1ea1730c0b08c1f590a799145143e2bf
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2018-08-06 13:36:32 -0500

    antiDeriv class updated.

--- src/mathLib/integral/AntiDerive.java
+++ src/mathLib/integral/AntiDerive.java
@@ -1,13 +1,27 @@
 package mathLib.integral;
 
+import mathLib.polynom.Polynomial;
+
 public class AntiDerive {
 	
 	public static double getSin(double x) {
-		return Math.cos(x) ;
+		return -Math.cos(x) ;
 	}
 	
 	public static double getCos(double x) {
-		return -Math.sin(x) ;
+		return Math.sin(x) ;
+	}
+	
+	public static double getTan(double x) {
+		return Math.log(1/Math.cos(x)) ;
+	}
+	
+	public static double getCot(double x) {
+		return Math.log(Math.sin(x)) ;
+	}
+	
+	public static double getPolynomial(Polynomial p, double x) {
+		return p.integrate().evaluate(x) ;
 	}
 	
 }
