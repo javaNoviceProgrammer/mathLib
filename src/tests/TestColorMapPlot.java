02bc57c38ef86b178213c7163e9527aff8d7ee7e
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2018-08-13 09:52:13 -0500

    minor changes.

--- src/tests/TestColorMapPlot.java
+++ src/tests/TestColorMapPlot.java
@@ -10,13 +10,13 @@ import static edu.uta.futureye.function.FMath.*;
 
 public class TestColorMapPlot {
 	public static void main(String[] args) {
-		double[] xVal = MathUtils.linspace(-5*Math.PI, 5*Math.PI, 1000) ;
+		double[] xVal = MathUtils.linspace(-5*Math.PI, 5*Math.PI, 200) ;
 		double[] yVal = MathUtils.linspace(0.0, 10.0, 1000) ;
 		MeshGrid grid = new MeshGrid(xVal, yVal) ;
-		double[][] fVal = new double[yVal.length][xVal.length] ;
+		double[][] fVal = new double[xVal.length][yVal.length] ;
 		MathFunc f = sin(x)*cos(y) ;
-		for(int i=0; i<yVal.length; i++) {
-			for(int j=0; j<xVal.length; j++) {
+		for(int i=0; i<xVal.length; i++) {
+			for(int j=0; j<yVal.length; j++) {
 				fVal[i][j] = f.apply(grid.getX(i, j), grid.getY(i, j)) ;
 			}
 		}
