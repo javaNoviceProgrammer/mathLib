7116a56ad187c60bfc4304bff63cb5a7a9a7b1c6
javaNoviceProgrammer <meisam@c-73-211-150-252.hsd1.il.comcast.net>
2018-08-13 19:22:09 -0400

    minor change to stringutil class

--- src/mathLib/util/StringUtils.java
+++ src/mathLib/util/StringUtils.java
@@ -1,5 +1,6 @@
 package mathLib.util;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import mathLib.numbers.Complex;
@@ -17,6 +18,20 @@ public class StringUtils {
 		} else {
 			return Double.NaN;
 		}
+	}
+
+	public static String fixedWidthDoubletoString(double var0, int var2, int var3) {
+		DecimalFormat var4 = new DecimalFormat();
+		var4.setMaximumFractionDigits(var3);
+		var4.setMinimumFractionDigits(var3);
+		var4.setGroupingUsed(false);
+
+		String var5;
+		for (var5 = var4.format(var0); var5.length() < var2; var5 = " " + var5) {
+			;
+		}
+
+		return var5;
 	}
 
 	public static double[] toDoubleArray(String s) {
