6c4722d825c43ba742c0a6d6c8a0a54e041f5c88
javaNoviceProgrammer <meisam@c-73-211-150-252.hsd1.il.comcast.net>
2018-09-01 15:50:44 -0400

    harmonic numbers added.

---
+++ src/mathLib/numbers/special/HarmonicNumber.java
@@ -0,0 +1,42 @@
+package mathLib.numbers.special;
+
+import mathLib.sequence.SumFunction;
+import mathLib.sequence.Summation;
+import plotter.chart.MatlabChart;
+import static mathLib.util.MathUtils.* ;
+
+import java.awt.Color;
+
+public class HarmonicNumber {
+
+	public static double harmonic (int n) {
+		if(n<1)
+			throw new IllegalArgumentException("argument must be greater than or equal to 1") ;
+
+		SumFunction func = new SumFunction() {
+			@Override
+			public double value(int k) {
+				return 1.0/k;
+			}
+		};
+
+		Summation sum = new Summation(func) ;
+		return sum.evaluate(1, n) ;
+	}
+
+	// for test
+	public static void main(String[] args) {
+		MatlabChart fig = new MatlabChart() ;
+		double[] x = linspace(1, 100, 1.0) ;
+		double[] y = new double[x.length] ;
+		for(int i=0; i<x.length; i++) {
+			y[i] = harmonic((int) x[i]) ;
+		}
+		fig.plot(x, y);
+		fig.RenderPlot();
+		fig.setAllColors(Color.RED);
+		fig.markerON();
+		fig.run(true);
+	}
+
+}
