c14ab69d94238388e9014d0f2002865a0d3f9097
javaNoviceProgrammer <meisam@c-73-211-150-252.hsd1.il.comcast.net>
2018-09-01 15:25:54 -0400

    minor change.

--- src/tests/TestComplexLinearSystem.java
+++ src/tests/TestComplexLinearSystem.java
@@ -2,15 +2,18 @@ package tests;
 
 import static mathLib.numbers.Complex.j;
 
+import mathLib.matrix.ComplexMatrix;
 import mathLib.matrix.sys.ComplexLinearSystem;
 import mathLib.numbers.Complex;
 import mathLib.util.Timer;
 
 public class TestComplexLinearSystem {
 	public static void main(String[] args) {
-		Complex[][] A = new Complex[][] {{1-j, 2*j, 3.0,1}, {0.5+j/2, j+1, 2,1}, {1, 1, 1,1}, {2,2,2,3}} ;
-		Complex[] y = new Complex[] {2.0, 3*j, j-2, -1} ;
-		Complex[] solution = new Complex[4] ;
+//		Complex[][] A = new Complex[][] {{1-j, 2*j, 3.0,1}, {0.5+j/2, j+1, 2,1}, {1, 1, 1,1}, {2,2,2,3}} ;
+		Complex[][] A = ComplexMatrix.identity(1000).times(2+j).getData() ;
+//		Complex[] y = new Complex[] {2.0, 3*j, j-2, -1} ;
+		Complex[] y = ComplexMatrix.constant(1, 1000, j/2).getData()[0] ;
+		Complex[] solution = new Complex[y.length] ;
 		Timer timer = new Timer() ;
 		timer.start();
 		new ComplexLinearSystem(A, y, solution) ;
