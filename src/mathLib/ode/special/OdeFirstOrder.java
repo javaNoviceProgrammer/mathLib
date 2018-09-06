f6674dbe47a30a7935160bfadfecfe552728009f
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2018-09-06 11:22:17 -0500

    ode's are added.

--- src/mathLib/ode/special/OdeFirstOrder.java
+++ src/mathLib/ode/special/OdeFirstOrder.java
@@ -1,5 +1,65 @@
 package mathLib.ode.special;
 
-public class OdeFirstOrder {
+import edu.uta.futureye.function.intf.MathFunc;
+import flanagan.integration.RungeKutta;
+import mathLib.ode.intf.DerivFunction1D;
+import mathLib.util.MathUtils;
+import plotter.chart.MatlabChart;
+
+import static edu.uta.futureye.function.FMath.* ;
+
+/**
+ * solving ordinary differential equation of the form  a(x) y' + b(x) y = f(x)
+ * 
+ *	a(x) cannot be null, but b(x) and f(x) can be null.
+ */
+
+public class OdeFirstOrder extends RungeKutta implements DerivFunction1D {
+	
+	MathFunc a, b, f ;
+	
+	public OdeFirstOrder(MathFunc a, MathFunc b, MathFunc f) {
+		if(a == null)
+			throw new IllegalArgumentException("First argument cannot be null") ;
+		if(b == null)
+			b = C(0) ;
+		if(f == null)
+			f = C(0) ;
+		
+		this.a = a ;
+		this.b = b ;
+		this.f = f ;
+	}
+	
+	@Override
+	public double deriv(double x, double y) {
+		// y' = g(x,y)
+		double g = (f.apply(x)-b.apply(x)*y)/a.apply(x) ;
+		return g;
+	}
+	
+	// for test
+	public static void main(String[] args) {
+		// solving y' = 2x on [0,2] with y(0) = 0
+		OdeFirstOrder ode = new OdeFirstOrder(C(1), null, 2*x) ;
+		ode.setInitialValueOfX(0);
+		ode.setInitialValueOfY(0);
+		ode.setStepSize(1e-4);
+		double[] xVal = MathUtils.linspace(0, 2, 100) ;
+		double[] yVal = new double[xVal.length] ;
+		for(int i=0; i<xVal.length; i++) {
+			ode.setFinalValueOfX(xVal[i]);
+			yVal[i] = ode.fourthOrder(ode) ;
+			
+		}
+		
+		MatlabChart fig = new MatlabChart() ;
+		fig.plot(xVal, yVal);
+		fig.RenderPlot();
+		fig.markerON();
+		fig.xlabel("X");
+		fig.ylabel("Solution Y");
+		fig.run(true);
+	}
 
 }
--- src/mathLib/ode/special/OdeSecondOrder.java
+++ src/mathLib/ode/special/OdeSecondOrder.java
@@ -1,5 +1,80 @@
 package mathLib.ode.special;
 
-public class OdeSecondOrder {
+import static edu.uta.futureye.function.FMath.C;
+import static edu.uta.futureye.function.FMath.*;
+
+import edu.uta.futureye.function.intf.MathFunc;
+import flanagan.integration.RungeKutta;
+import mathLib.ode.intf.DerivnFunction1D;
+import mathLib.util.MathUtils;
+import plotter.chart.MatlabChart;
+
+/**
+ * solving ordinary differential equation of the form  a(x) y'' + b(x) y' + c(x) y = f(x)
+ * 
+ *	a(x) cannot be null, but b(x), c(x) and f(x) can be null.
+ *
+ *	solution gives y(x) and y'(x)
+ */
+
+public class OdeSecondOrder extends RungeKutta implements DerivnFunction1D {
+
+	MathFunc a, b, c, f ;
+	
+	public OdeSecondOrder(MathFunc a, MathFunc b, MathFunc c, MathFunc f) {
+		if(a == null)
+			throw new IllegalArgumentException("First argument cannot be null") ;
+		if(b == null)
+			b = C(0) ;
+		if(c == null)
+			c = C(0) ;
+		if(f == null)
+			f = C(0) ;
+		
+		this.a = a ;
+		this.b = b ;
+		this.c = c ;
+		this.f = f ;
+	}
+	
+	@Override
+	public double[] derivn(double x, double[] yy) {
+		// z = y' => y'' = z' 
+		// y' = g0(x,y,z)
+		// z' = g1(x,y,z)
+		double y = yy[0] ;
+		double z = yy[1] ;
+		double g0 = z ;
+		double g1 = f.apply(x) - c.apply(x)*y - b.apply(x) * z ;
+		g1 = g1/a.apply(x) ;
+		return new double[] {g0, g1} ;
+	}
+	
+	// for test
+	public static void main(String[] args) {
+		// solving y'' = 2 sin(x) on [0,20] with y(0) = 0, y'(0) = 0
+		OdeSecondOrder ode = new OdeSecondOrder(C(1), null, null, 2*sin(x)) ;
+		ode.setInitialValueOfX(0);
+		ode.setInitialValueOfY(new double[] {0,0});
+		ode.setStepSize(1e-1);
+		double[] xVal = MathUtils.linspace(0, 20, 100) ;
+		double[] yVal = new double[xVal.length] ;
+		double[] dyVal = new double[xVal.length] ;
+		for(int i=0; i<xVal.length; i++) {
+			ode.setFinalValueOfX(xVal[i]);
+			yVal[i] = ode.fourthOrder(ode)[0] ;
+			dyVal[i] = ode.fourthOrder(ode)[1] ;
+		}
+		
+		MatlabChart fig = new MatlabChart() ;
+		fig.plot(xVal, yVal, "b");
+//		fig.plot(xVal, dyVal, "r", 2, "y_prime");
+		fig.RenderPlot();
+		fig.markerON();
+		fig.xlabel("X");
+		fig.ylabel("Solution Y");
+		fig.run(true);
+	}
+	
 
 }
