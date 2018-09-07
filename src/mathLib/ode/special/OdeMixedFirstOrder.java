6dfd7c69f4d1c75e7f4625285af0529806079adc
javaNoviceProgrammer <meisam@c-73-211-150-252.hsd1.il.comcast.net>
2018-09-06 22:29:54 -0400

    minor change.

--- src/mathLib/ode/special/OdeMixedFirstOrder.java
+++ src/mathLib/ode/special/OdeMixedFirstOrder.java
@@ -13,7 +13,7 @@ import plotter.chart.MatlabChart;
 
 /**
  * Solves the differential equation of form a(x,y) y' + b(x,y) = f(x,y)
- * 
+ *
  * a(x,y) cannot be null, but b(x,y) and f(x,y) can be null.
  *
  */
@@ -21,7 +21,7 @@ import plotter.chart.MatlabChart;
 public class OdeMixedFirstOrder extends RungeKutta implements DerivFunction1D {
 
 	MathFunc a, b, f ;
-	
+
 	public OdeMixedFirstOrder(MathFunc a, MathFunc b, MathFunc f) {
 		if(a == null)
 			throw new IllegalArgumentException("First argument cannot be null") ;
@@ -29,12 +29,12 @@ public class OdeMixedFirstOrder extends RungeKutta implements DerivFunction1D {
 			b = C(0) ;
 		if(f == null)
 			f = C(0) ;
-		
+
 		this.a = a ;
 		this.b = b ;
 		this.f = f ;
 	}
-	
+
 	@Override
 	public double deriv(double x, double y) {
 		// y' = g0(x,y)
@@ -42,11 +42,11 @@ public class OdeMixedFirstOrder extends RungeKutta implements DerivFunction1D {
 		g0 = g0/a.apply(x,y) ;
 		return g0;
 	}
-	
+
 	// for test
 	public static void main(String[] args) {
 		// solving: y'(x) = sin(x*x)-y with y(0) = 0
-		OdeMixedFirstOrder ode = new OdeMixedFirstOrder(C(1), null, sin(x*x)-y) ;
+		OdeMixedFirstOrder ode = new OdeMixedFirstOrder(C(1), null, sin(x*x)-y*y) ;
 		ode.setInitialValueOfX(0);
 		ode.setInitialValueOfY(0);
 		ode.setStepSize(1e-1);
@@ -56,7 +56,7 @@ public class OdeMixedFirstOrder extends RungeKutta implements DerivFunction1D {
 			ode.setFinalValueOfX(xVal[i]);
 			yVal[i] = ode.fourthOrder(ode) ;
 		}
-		
+
 		MatlabChart fig = new MatlabChart() ;
 		fig.plot(xVal, yVal, "b");
 		fig.RenderPlot();
