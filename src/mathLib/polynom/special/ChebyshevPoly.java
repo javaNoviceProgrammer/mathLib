94c7670c19431ef2b0d8468974de1fb5a8730a12
javaNoviceProgrammer <meisam@c-73-211-150-252.hsd1.il.comcast.net>
2018-09-09 09:00:32 -0400

    minor change.

--- src/mathLib/polynom/special/ChebyshevPoly.java
+++ src/mathLib/polynom/special/ChebyshevPoly.java
@@ -1,10 +1,8 @@
 package mathLib.polynom.special;
 
-import static mathLib.polynom.Polynomial.*;
+import static mathLib.polynom.ComplexPolynomial.Xc;
+import static mathLib.polynom.Polynomial.X;
 
-import mathLib.numbers.Complex;
-
-import static mathLib.polynom.ComplexPolynomial.*;
 import mathLib.polynom.ComplexPolynomial;
 import mathLib.polynom.Polynomial;
 
@@ -17,10 +15,10 @@ public class ChebyshevPoly {
 			return X ;
 		else if(n >= 2)
 			return 2*X*firstKind(n-1)-firstKind(n-2) ;
-		else 
+		else
 			throw new IllegalArgumentException("degree must be 0 or higher") ;
 	}
-	
+
 	public static Polynomial secondKind(int n) {
 		if(n==0)
 			return 0*X + 1 ;
@@ -28,24 +26,24 @@ public class ChebyshevPoly {
 			return 2*X ;
 		else if(n >= 2)
 			return 2*X*secondKind(n-1)-secondKind(n-2) ;
-		else 
+		else
 			throw new IllegalArgumentException("degree must be 0 or higher") ;
 	}
-	
+
 	/**
 	 * Complex version of the Chebyshev polynomial
 	 * @param n
 	 * @return
 	 */
-	
+
 	public static ComplexPolynomial firstKindC(int n) {
 		return firstKind(n)+0*Xc ;
 	}
-	
+
 	public static ComplexPolynomial secondKindC(int n) {
 		return secondKind(n)+0*Xc ;
 	}
-	
+
 	// for test
 	public static void main(String[] args) {
 		System.out.println(firstKind(0));
@@ -53,19 +51,25 @@ public class ChebyshevPoly {
 		System.out.println(firstKind(2));
 		System.out.println(firstKind(3));
 		System.out.println(firstKind(4));
-		System.out.println(firstKindC(4)*Complex.j);
-		
-		System.out.println("\n \n");
-		
-		System.out.println(secondKind(0));
-		System.out.println(secondKind(1));
-		System.out.println(secondKind(2));
-		System.out.println(secondKind(3));
-		System.out.println(secondKind(4));
-		
-		System.out.println("\n"+(firstKind(1)*firstKind(2)).integrate());
-		
+		System.out.println(firstKind(5));
+		System.out.println(firstKind(6));
+		System.out.println(firstKind(7));
+		System.out.println(firstKind(8));
+		System.out.println(firstKind(9));
+		System.out.println(firstKind(10));
+		System.out.println(firstKind(11));
+
+//		System.out.println("\n \n");
+//
+//		System.out.println(secondKind(0));
+//		System.out.println(secondKind(1));
+//		System.out.println(secondKind(2));
+//		System.out.println(secondKind(3));
+//		System.out.println(secondKind(4));
+//
+//		System.out.println("\n"+(firstKind(1)*firstKind(2)).integrate());
+
 	}
-	
-	
+
+
 }
