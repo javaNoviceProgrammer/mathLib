499b260e4515d31a4210646bc86e3b1c14cff777
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2018-10-23 17:28:12 -0500

    object utils added.

---
+++ src/mathLib/util/ObjectUtils.java
@@ -0,0 +1,49 @@
+package mathLib.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class ObjectUtils {
+	
+	public static void writeToFile(File file) {
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(file);
+			ObjectOutputStream oos = new ObjectOutputStream(fos) ;
+			oos.writeObject(oos);
+			fos.close();
+			oos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Object readFromFile(File file) {
+		FileInputStream fis = null;
+		Object readObject = null ;
+		try {
+			fis = new FileInputStream(file);
+			ObjectInputStream ois = new ObjectInputStream(fis) ;
+			
+			try {
+				readObject = ois.readObject() ;
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+			fis.close();
+			ois.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return readObject ;
+	}
+	
+	
+
+}
