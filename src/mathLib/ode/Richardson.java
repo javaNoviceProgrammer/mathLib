1d9b7ef929640c8aa5e983bf80ceddfe812964fa
javaNoviceProgrammer <meisam@c-73-211-150-252.hsd1.il.comcast.net>
2018-11-12 09:52:11 -0600

    minor change to richardson extrapolation

--- src/mathLib/ode/Richardson.java
+++ src/mathLib/ode/Richardson.java
@@ -1,33 +1,34 @@
 package mathLib.ode;
 
+import edu.uta.futureye.function.intf.MathFunc;
 import mathLib.func.intf.RealFunction;
+import static edu.uta.futureye.function.FMath.*;
 
 public class Richardson {
 
-	public static double deriv(RealFunction func, double x, int n) {
-		double eps = 1e-3 ;
-		double h = eps * x ;
-
-		return extrapolate(func, x, n, h) ;
+	public static double deriv(RealFunction func, double x, double dx, int n) {
+		return extrapolate(func, x, n, dx) ;
 	}
 
 	private static double extrapolate(RealFunction func, double x, int n, double h) {
 		if(n<0)
 			throw new IllegalArgumentException("n must be greater than or equal to 0") ;
 		if(n==0)
-			return (func.evaluate(x+h)-func.evaluate(x))/h ;
-		return (Math.pow(2, n) *extrapolate(func, x, n-1, h/Math.pow(2, n)) - extrapolate(func, x, n-1, h))/(Math.pow(2, n)-1) ;
+			return (func.evaluate(x+h)-func.evaluate(x-h))/(2.0*h) ; // order h^2
+		return (Math.pow(2, n+1) *extrapolate(func, x, n-1, h/2.0) - extrapolate(func, x, n-1, h))/(Math.pow(2, n+1)-1) ;
 	}
 
 	// for test
 	public static void main(String[] args) {
-		RealFunction func = t -> Math.tan(t*t) ;
-		System.out.println(deriv(func, 1, 0));
-		System.out.println(deriv(func, 1, 1));
-		System.out.println(deriv(func, 1, 2));
-		System.out.println(deriv(func, 1, 3));
-		System.out.println(deriv(func, 1, 4));
-		System.out.println(deriv(func, 1, 5));
+		RealFunction func = t -> (t*Math.sin(t*t)/Math.cos(t)) ;
+		MathFunc f = x*sin(x*x)/cos(x) ;
+		System.out.println("Exact deriv=      " + f.diff("x").apply(2.0));
+		System.out.println("zeroth iteration: " +  deriv(func, 2, 1e-1, 0));
+		System.out.println("first iteration:  "  + deriv(func, 2, 1e-1, 1));
+		System.out.println("second iteration: " +  deriv(func, 2, 1e-1, 2));
+		System.out.println("third iteration:  "  + deriv(func, 2, 1e-1, 3));
+		System.out.println("forth iteration:  "  + deriv(func, 2, 1e-1, 4));
+		System.out.println("fifth iteration:  "  + deriv(func, 2, 1e-1, 5));
 	}
 
 }
