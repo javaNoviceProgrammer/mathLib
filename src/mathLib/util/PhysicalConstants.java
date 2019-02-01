6f3b0637a52794eac67cf1fc5c9876747d9d22bc
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2019-02-01 10:44:36 -0600

    minor change.

--- src/mathLib/util/PhysicalConstants.java
+++ src/mathLib/util/PhysicalConstants.java
@@ -10,63 +10,63 @@ public class PhysicalConstants {
 
 	// MKS system (SI units)
 
-	public static double getAvogadroNumber(){
+	public static double AvogadroNumber(){
 		return 6.02214e23 ;
 	}
 
-	public static double getBohrRadius(){
+	public static double BohrRadius(){
 		return 0.52917e-10;
 	}
 
-	public static double getBoltzmannConstant(){
+	public static double BoltzmannConstant(){
 		return 1.38066e-23;
 	}
 
-	public static double getElementaryCharge(){
+	public static double ElementaryCharge(){
 		return 1.60218e-19;
 	}
 
-	public static double getElectronRestMass(){
+	public static double ElectronRestMass(){
 		return 0.91094e-30;
 	}
 
-	public static double getElectronVolt(){
+	public static double ElectronVolt(){
 		return 1.60218e-19;
 	}
 
-	public static double getGasConstant(){
+	public static double GasConstant(){
 		return 1.98719;
 	}
 
-	public static double getPermittivityVacuum(){
+	public static double PermittivityVacuum(){
 		return 8.85418e-12;
 	}
 
-	public static double getPermeabilityVacuum(){
+	public static double PermeabilityVacuum(){
 		return (4*Math.PI*1e-7);
 	}
 
-	public static double getPlanckConstant(){
+	public static double PlanckConstant(){
 		return 6.62607e-34;
 	}
 
-	public static double getReducedPlanckConstant(){
+	public static double ReducedPlanckConstant(){
 		return 1.05457e-34;
 	}
 
-	public static double getProtonRestMass(){
+	public static double ProtonRestMass(){
 		return 1.67262e-27;
 	}
 
-	public static double getSpeedOfLightVacuum(){
+	public static double SpeedOfLightVacuum(){
 		return 2.99792e8;
 	}
 
-	public static double getStandardAtmoshpere(){
+	public static double StandardAtmoshpere(){
 		return 1.01325e5;
 	}
 
-	public static double getThermalVoltageAt300K(){
+	public static double ThermalVoltageAt300K(){
 		return 0.025852;
 	}
 
