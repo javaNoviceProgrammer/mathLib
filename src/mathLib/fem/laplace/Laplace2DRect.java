23db75f49d11966b91381dae373f001d83957f46
javaNoviceProgrammer <meisam@192.168.2.14>
2019-06-15 07:19:15 -0400

    minor change.

--- src/mathLib/fem/laplace/Laplace2DRect.java
+++ src/mathLib/fem/laplace/Laplace2DRect.java
@@ -1,10 +1,31 @@
 package mathLib.fem.laplace;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
+import mathLib.fem.FEMUtils;
+import mathLib.fem.assembler.Assembler;
 import mathLib.fem.core.Mesh;
-import mathLib.fem.core.Node;
-import mathLib.fem.mesh.mesh2d.Mesh2DRectangleElement;
-import mathLib.fem.mesh.mesh2d.Mesher2D;
+import mathLib.fem.core.NodeType;
+import mathLib.fem.core.intf.FiniteElement;
+import mathLib.fem.element.FELinearTriangle;
+import mathLib.fem.mesh.Mesh2DRect;
+import mathLib.fem.util.Utils;
+import mathLib.fem.weakform.WeakForm;
+import mathLib.func.ArrayFunc;
+import mathLib.func.symbolic.MultiVarFunc;
+import mathLib.func.symbolic.basic.FX;
+import mathLib.func.symbolic.intf.MathFunc;
+import mathLib.geometry.algebra.Point;
+import mathLib.matrix.algebra.intf.Matrix;
+import mathLib.matrix.algebra.intf.Vector;
+import mathLib.matrix.algebra.solver.Solver;
+import mathLib.plot.ColorMapPlot;
+import mathLib.plot.util.MeshGrid;
+import mathLib.util.MathUtils;
+import mathLib.util.Timer;
+
 
 public class Laplace2DRect {
 
@@ -14,28 +35,28 @@ public class Laplace2DRect {
 
 	public static void main(String[] args) {
 
-//		Mesh2DRect mesh2d = new Mesh2DRect(Point.getInstance(0, 0), Point.getInstance(1, 1), 50, 50) ;
-//		Mesh mesh = mesh2d.getMesh() ;
+		Mesh2DRect mesh2d = new Mesh2DRect(Point.getInstance(0, 0), Point.getInstance(1, 1), 50, 50) ;
+		Mesh mesh = mesh2d.getMesh() ;
 		
-		Mesh2DRectangleElement rect1 = new Mesh2DRectangleElement("rect1", 0.0, 0.0, 1.0, 1.0) ;
-		Mesher2D mesher2d = new Mesher2D() ;
-		mesher2d.addElement(rect1);
-		mesher2d.triangulate();
-		mesher2d.getCanvas().run(true);
-		mesher2d.showNodeNumbers(1e-2, 1e-2);
-		Mesh mesh = mesher2d.getMesh() ;
+//		Mesh2DRectangleElement rect1 = new Mesh2DRectangleElement("rect1", 0.0, 0.0, 1.0, 1.0) ;
+//		Mesher2D mesher2d = new Mesher2D() ;
+//		mesher2d.addElement(rect1);
+//		mesher2d.triangulate();
+//		mesher2d.getCanvas().run(true);
+//		mesher2d.showNodeNumbers(1e-2, 1e-2);
+//		Mesh mesh = mesher2d.getMesh() ;
+//		
+//		System.out.println(mesh.getNodeList().size());
+//
+//		System.out.println(mesh.getNodeList().at(51));
+//		System.out.println(mesh.getNodeList().at(52));
+//		
+//		Node node51 = mesh.getNodeList().at(51) ;
+//		Node node52 = mesh.getNodeList().at(52) ;
+//		Node node = new Node(52, node51.coord(1), node51.coord(2)) ;
+//		System.out.println(node51.equals(node52));
 		
-		System.out.println(mesh.getNodeList().size());
-
-		System.out.println(mesh.getNodeList().at(51));
-		System.out.println(mesh.getNodeList().at(52));
 		
-		Node node51 = mesh.getNodeList().at(51) ;
-		Node node52 = mesh.getNodeList().at(52) ;
-		Node node = new Node(52, node51.coord(1), node51.coord(2)) ;
-		System.out.println(node51.equals(node52));
-		
-		/*
 		// Compute geometry relationship between nodes and elements
 		mesh.computeNodeBelongsToElements();
 		// 2.Mark boundary type(s)
@@ -45,7 +66,7 @@ public class Laplace2DRect {
 
 		// 3. Weak form definition
 		FiniteElement fe = new FELinearTriangle() ; // Linear triangular finite element
-		MathFunc f = 0*x+0*y ; //Right hand side (RHS)
+		MathFunc f = 0*FX.x+0*FX.y ; //Right hand side (RHS)
 		WeakForm wf = new WeakForm(fe,
 				(u,v) -> -u.diff("x")*v.diff("x")-u.diff("y")*v.diff("y") ,
 				v -> f * v
@@ -69,9 +90,9 @@ public class Laplace2DRect {
 				if(Math.abs(y)<eps)
 					return 0.0 ;
 				else if(Math.abs(y-1)<eps)
-					return Math.sin(Math.PI/2.0 * x) ;
+					return Math.sin(Math.PI/1.0 * x) ;
 				else if(Math.abs(x)<eps)
-					return Math.sin(Math.PI/2.0 * y) ;
+					return 0.0 ;
 				else
 					return 0.0;
 			}
@@ -93,19 +114,21 @@ public class Laplace2DRect {
 		System.out.println(timer);
 
 		// 6. Output the result to a MATLAB chart
-		double[] x = MathUtils.linspace(0.0, 1.0, 100) ;
-		double[] y = MathUtils.linspace(0.0, 1.0, 100) ;
-		MeshGrid grid = new MeshGrid(x, y) ;
-
+//		double[] x = MathUtils.linspace(0.0, 1.0, 100) ;
+//		double[] y = MathUtils.linspace(0.0, 1.0, 100) ;
+//		MeshGrid grid = new MeshGrid(x, y) ;
+		
+//		MeshGrid grid = mesh2d.getGrid() ;
+//
 //		FEMUtils.plotResult(grid, u);
-		FEMUtils.plotResultDense(grid, u, 5, 5);
-
+//		FEMUtils.plotResultDense(grid, u, 5, 5);
+//
 //		double[][] solExact = ArrayFunc.apply((a,b)-> Math.sin(Math.PI*a)*Math.sinh(Math.PI*b)/Math.sinh(Math.PI) , grid) ;
 //		ColorMapPlot figExact = new ColorMapPlot(grid, solExact) ;
 //		figExact.run(true);
-  
+//  
  
- */
+ 
 
 	}
 
