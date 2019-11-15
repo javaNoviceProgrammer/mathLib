196d719194c3b0c9224c2697d1e623f967ab7bde
javaNoviceProgrammer <meisam@192.168.2.14>
2019-11-15 18:59:24 -0500

    legendre polynomial modified.

--- src/mathLib/polynom/special/LegendrePoly.java
+++ src/mathLib/polynom/special/LegendrePoly.java
@@ -2,6 +2,7 @@ package mathLib.polynom.special;
 
 import static mathLib.polynom.ComplexPolynomial.Xc;
 import static mathLib.polynom.Polynomial.X;
+import static mathLib.polynom.Polynomial.x;
 import static mathLib.util.MathUtils.factorial;
 
 import mathLib.polynom.ComplexPolynomial;
@@ -9,6 +10,29 @@ import mathLib.polynom.Polynomial;
 import mathLib.util.MathUtils;
 
 public class LegendrePoly {
+	
+	public static Polynomial fromBonnet(int degree) {
+		// stop condition
+		if(degree == 0)
+			return 1.0 + 0.0*x ;
+		if(degree == 1)
+			return x ;
+		// return
+		int n = degree - 1 ;
+		Polynomial result = ((2.0*n+1.0)*x*fromBonnet(n) - n*fromBonnet(n-1))/(n+1) ;
+		return result.reduce() ;
+	}
+
+	public static Polynomial fromRodrigues(int degree) {
+		// step 1: (x^2-1)^n
+		Polynomial base = (x*x-1).pow(degree) ;
+		// step 2: d^n/dx^n (x^2-1)^n
+		Polynomial diffBase = base.diff(degree) ;
+		// step 3: 2^n * n!
+		double coeff = Math.pow(2.0, degree) * MathUtils.factorial(degree) ;
+		// return the result
+		return (diffBase/coeff).reduce() ;
+	}
 
 	public static Polynomial legendre(int degree) {
 		if(degree == 0)
